b671d6afd4a7d174246fddcf4c79d57c24141b91
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2023-12-01 22:44:17 +0700

    refactor: membuat navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import Image from 'next/image'
-
 export default function Home() {
-  return (
-    <h1>
-      Hello World
-    </h1>
-  )
+  return <h1 className="font-bold text-teal-500 text-2xl">Hello World</h1>;
 }
---
+++ components/Button.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+type ButtonProps = {
+  type: "submit" | "button";
+  title: string;
+  icon?: string;
+  variant: "btn_dark_green";
+};
+
+const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      className={`flexCenter gap-3 border rounded-full ${variant}`}>
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}
+      <label className="bold-16 whitespace-nowrap">{title}</label>
+    </button>
+  );
+};
+
+export default Button;
