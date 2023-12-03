d1b8415f5c7aeee0199bb5c98aa0bbfa6f34195b
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2023-12-03 20:15:53 +0700

    style: formatting & wrapping image

--- components/Features.tsx
+++ components/Features.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 const Features = () => {
   return (
-    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-20">
-      <div className="max-container padding-container w-full relative flex justify-end">
+    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full flex justify-end">
         <div className="flex flex-1 lg:min-h-[900px]">
           <Image
             src="/phone.png"
@@ -17,26 +17,27 @@ const Features = () => {
         </div>
 
         <div className="z-20 flex w-full flex-col lg:w-[60%]">
-          <Image
-            src="/camp.svg"
-            alt="camp"
-            width={50}
-            height={50}
-            className="absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]"
-          />
-          <h2 className="bold-40 lg:bold-64">Our Features</h2>
-        </div>
-
-        <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mt-20 lg:gap-20">
-          {FEATURES.map((features) => (
-            <FeatureItem
-              key={features.title}
-              title={features.title}
-              icon={features.icon}
-              description={features.description}
+          <div className="relative">
+            <Image
+              src="/camp.svg"
+              alt="camp"
+              width={50}
+              height={50}
+              className="absolute left-[-5px] top-[-28px] w-10 lg:w-[50px]"
             />
-          ))}
-        </ul>
+            <h2 className="bold-40 lg:bold-64">Our Features</h2>
+          </div>
+          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mg-20 lg:gap-20">
+            {FEATURES.map((feature) => (
+              <FeatureItem
+                key={feature.title}
+                title={feature.title}
+                icon={feature.icon}
+                description={feature.description}
+              />
+            ))}
+          </ul>
+        </div>
       </div>
     </section>
   );
