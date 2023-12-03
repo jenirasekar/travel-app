ffd548609460f48cbdc74f78cc41347a6227a689
Jenira Sekar Az-zahra <jenirasekar@gmail.com>
2023-12-03 20:33:29 +0700

    styling: formatting

--- components/Button.tsx
+++ components/Button.tsx
@@ -12,9 +12,13 @@ const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 border rounded-full ${variant} ${full && 'w-full'}`}>
+      className={`flexCenter gap-3 border rounded-full ${variant} ${
+        full && "w-full"
+      }`}>
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
+      <label className="bold-16 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
     </button>
   );
 };
